267dd17f97c98a0d0332e97cadc7c9577fe05582
S.Piyapol <tpyostm@gmail.com>
2022-09-07 10:58:48 +0700

    07-09-2022

--- App.js
+++ App.js
@@ -1,33 +1,87 @@
-import {Text, View, Button,TextInput } from "react-native";
-// import HomeScreen from "./screen/HomeScreen";
-
-import FirstPage from "./FirstPage";
-import SecondPage from "./SecoundPage";
-import ThirdPage from "./ThirdPage";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import React from "react";
 
-const Stack = createNativeStackNavigator();
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import HomeScreen from "./HomeScreen";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { SafeAreaView } from "react-native-web";
 
-export default function App() {{
-    return (
-    <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName='FirstPage'
-      screenOptions={{
-        headerStyle: { backgroundColor: '#ff8c00' },
-        headerTintColor: '#ffff',
-        headerTitleStyle: { fontWeight: 'bold', fontSize: 30 },
-      }}
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,15,85)",
+  },
+};
+
+function FeedScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Feed Screen</Text>
+      <Button onPress={() => navigation.openDrawer()} title="Home Screen" />
+    </View>
+  );
+}
+
+function NortiScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Nortification Screen</Text>
+    </View>
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView>
+      <Image
+        style={styles.sideMenuProfileIcon}
+        source={require("./assets/react_logo.png")}
+      />
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+                <DrawerItem
+          label="Close Drawer"
+          onPress={() => props.navigation.closeDrawer()}
+        />
+      </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+}
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
     >
-      <Stack.Screen name='First Page' component={FirstPage} />
-      <Stack.Screen name='Second Page' component={SecondPage} />
-      <Stack.Screen name='Third Page' component={ThirdPage} />
-      
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Nortification" component={NortiScreen} />
+    </Drawer.Navigator>
+  );
 }
-}
+const App = () => {
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: "center",
+  },
+});
+
+export default App;
--- HomeScreen.js
+++ HomeScreen.js
@@ -2,18 +2,43 @@ import { View, Text,Button } from 'react-native'
 import React from 'react'
 
 import { Ionicons } from '@expo/vector-icons';
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from 'react-navigation-header-buttons';
 
+import {} from 'react-navigation-header-buttons'
+
+const IoniconsHeaderButton = (props) => (
+  // the `props` here come from <Item ... />
+  // you may access them and pass something else to `HeaderButton` if you like
+  <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
 
 const HomeScreen = ({navigation}) => {
+  React.useLayoutEffect(() => {
+  navigation.setOptions({
+    // in your app, you can extract the arrow function into a separate component
+    // to avoid creating a new one every time you update the options
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+        <Item title="register" iconName="person-add" onPress={() => alert('ลงทะเบียน')} />
+      </HeaderButtons>
+    ),
+  });
+}, [navigation]);
+
+
   return (
     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
     <Ionicons name='home' size={30} color='#008bb8'/>
     <Text> Home Screen </Text>
     <Button 
-    title='เกี่ยวกับเรา' 
-    onPress={()=>navigation.navigate('About',{
-        email:'reactnative.tni.ac.th'
-    })}
+    title='Open Drawer' 
+    onPress={()=>navigation.openDrawer()}
     />
     </View>
   )
